f7147aaa48d7a0f2a03ba3c6851270373c413d6d
wangzhen <wangzhen@yunjiacloud.com>
2017-12-29 15:01:51 +0800

    配置文件

--- src/views/layout/Layout.vue
+++ src/views/layout/Layout.vue
@@ -1,5 +1,13 @@
 <template>
   <div class="app-wrapper" :class="{hideSidebar:!sidebar.opened}">
+    <nav id="nav">
+      <el-tabs class="navMenu" v-model="activeName" @tab-click="tabClick()">
+        <el-tab-pane label="首页" name="home"></el-tab-pane>
+        <el-tab-pane label="产品" name="product"></el-tab-pane>
+        <el-tab-pane label="控制台" name="console"></el-tab-pane>
+        <el-tab-pane label="开发者中心" name="developer"></el-tab-pane>
+      </el-tabs>
+    </nav>
     <sidebar class="sidebar-container"></sidebar>
     <div class="main-container">
       <navbar></navbar>
@@ -9,29 +17,55 @@
 </template>
 
 <script>
-import { Navbar, Sidebar, AppMain } from '@/views/layout/components'
+  import { Navbar, Sidebar, AppMain } from '@/views/layout/components'
 
-export default {
-  name: 'layout',
-  components: {
-    Navbar,
-    Sidebar,
-    AppMain
-  },
-  computed: {
-    sidebar() {
-      return this.$store.state.app.sidebar
+  export default {
+    name: 'layout',
+    data(){
+      return{
+        activeName: 'home'
+      }
+    },
+    components: {
+      Navbar,
+      Sidebar,
+      AppMain
+    },
+    methods:{
+      tabClick(tab, event) {
+        if(this.activeName == 'home'){
+          this.$router.push('/')
+        }else if(this.activeName == 'product'){
+          this.$router.push('/form/index')
+        }else if(this.activeName == 'console'){
+          this.$router.push('/example/table')
+        }else if(this.activeName == 'developer'){
+          this.$router.push('/example/table')
+        }
+      }
+    },
+    computed: {
+      sidebar() {
+        return this.$store.state.app.sidebar
+      }
     }
   }
-}
 </script>
 
 <style rel="stylesheet/scss" lang="scss" scoped>
-@import "src/styles/mixin.scss";
-.app-wrapper {
-  @include clearfix;
-  position: relative;
-  height: 100%;
-  width: 100%;
-}
+  @import "src/styles/mixin.scss";
+  .app-wrapper {
+    @include clearfix;
+    position: relative;
+    height: 100%;
+    width: 100%;
+    #nav {
+      height: 69px;
+      background-color: #E1EAEF;
+      box-shadow: 1px 1px #AEC0CA;
+      .navMenu {
+        width: 333px;
+      }
+    }
+  }
 </style>
