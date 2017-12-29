9749c8cfd6cb8f100f7085dfc917dcb676b77f8b
wangzhen <wangzhen@yunjiacloud.com>
2017-12-29 17:10:45 +0800

    右上下拉菜单

--- src/App.vue
+++ src/App.vue
@@ -7,18 +7,42 @@
         <el-tab-pane label="控制台" name="console"></el-tab-pane>
         <el-tab-pane label="开发者中心" name="developer"></el-tab-pane>
       </el-tabs>
+      <el-dropdown class="avatar-container" trigger="click" style="float:right;">
+        <div class="avatar-wrapper">
+          {{name}}
+          <i class="el-icon-caret-bottom"></i>
+        </div>
+        <el-dropdown-menu class="user-dropdown" slot="dropdown">
+          <router-link class="inlineBlock" to="/">
+            <el-dropdown-item>
+              Home
+            </el-dropdown-item>
+          </router-link>
+          <el-dropdown-item divided>
+            <span @click="logout" style="display:block;">LogOut</span>
+          </el-dropdown-item>
+        </el-dropdown-menu>
+      </el-dropdown>
     </nav>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
 export default {
   name: 'app',
   data(){
     return{
       activeName: 'home'
     }
+  },
+  computed: {
+    ...mapGetters([
+      'sidebar',
+      'avatar',
+      'name'
+    ])
   },
   methods:{
     tabClick(tab, event) {
@@ -31,6 +55,14 @@ export default {
       }else if(this.activeName == 'developer'){
         this.$router.push('/developer')
       }
+    },
+    toggleSideBar() {
+      this.$store.dispatch('ToggleSideBar')
+    },
+    logout() {
+      this.$store.dispatch('LogOut').then(() => {
+        location.reload() // 为了重新实例化vue-router对象 避免bug
+      })
     }
   }
 }
@@ -51,6 +83,7 @@ export default {
       z-index: 3;
       .navMenu {
         width: 333px;
+        float: left;;
       }
     }
   }
