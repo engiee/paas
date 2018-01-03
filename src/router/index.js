8f4b884c2dce2bc69c80eacaf5999db2fd0dc4d1
wangzhen <wangzhen@yunjiacloud.com>
2018-01-03 10:41:46 +0800

    router

--- src/router/index.js
+++ src/router/index.js
@@ -2,19 +2,18 @@ import Vue from 'vue'
 import Router from 'vue-router'
 const _import = require('./_import_' + process.env.NODE_ENV)
 // in development-env not use lazy-loading, because lazy-loading too many pages will cause webpack hot update too slow. so only in production use lazy-loading;
-// detail: https://panjiachen.github.io/vue-element-admin-site/#/lazy-loading
 
 Vue.use(Router)
 
 /* Layout */
 import Layout from '../views/layout/Layout'
 /**
-* hidden: true                   if `hidden:true` will not show in the sidebar(default is false)
-* redirect: noredirect           if `redirect:noredirect` will no redirct in the breadcrumb
-* name:'router-name'             the name is used by <keep-alive> (must set!!!)
+* hidden: true                   如果 `hidden:true` 侧边栏中不显示（默认为false）
+* redirect: noredirect           如果 `redirect:noredirect` 在面包屑导航里没有 redirct
+* name:'router-name'             在<keep-alive>用 (必须设置!!!)
 * meta : {
-    title: 'title'               the name show in submenu and breadcrumb (recommend set)
-    icon: 'svg-name'             the icon show in the sidebar,
+    title: 'title'               子菜单 & 面包屑导航 名称 (推荐设置)
+    icon: 'svg-name'             icon（icons文件夹里的svg名）
   }
 **/
 export const constantRouterMap = [
